fdc9c8a20a5307eb49edcd87019a778d88cff445
Yigal Duppen <yigal@publysher.nl>
2016-07-05 14:45:23 +0200

    TG-720 Refactor: niet langer hardcoden van indicator

--- web/zwaailicht/api/views.py
+++ web/zwaailicht/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from . import mapping
+
 
 class PandStatusViewSet(viewsets.ViewSet):
     """
     De pand status geeft een overzicht van eigenschappen van het pand.
     """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.mapping = mapping.Mapping()
 
     def list(self, request):
         return Response("Gebruik de url /status_pand/{bag_id} om gedetailleerde informatie terug te krijgen.")
@@ -16,11 +22,6 @@ class PandStatusViewSet(viewsets.ViewSet):
                 'bag_id': pk,
             },
             'indicatoren': [
-                {
-                    'indicator': 'Status pand',
-                    'waarschuwingsniveau': 1,
-                    'label': 'Beperking pand',
-                    'aanvullende_informatie': 'Splitsing pand zonder vergunning [Status]',
-                }
+                self.mapping.beperking_to_status_pand('HS'),
             ],
         })
