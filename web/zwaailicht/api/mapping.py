674df4a7c221acd83ecabd56bff60c083c19ba17
Yigal Duppen <yigal@publysher.nl>
2016-07-05 15:00:46 +0200

    TG-720 Add mapping for pand status

--- web/zwaailicht/api/mapping.py
+++ web/zwaailicht/api/mapping.py
@@ -11,12 +11,17 @@ class Mapping(object):
         with open('mapping.json') as f:
             self.mapping = json.load(f)
 
-    def beperking_to_status_pand(self, beperking_code):
-        result = self.mapping['Status pand']['Beperking.beperkingcode'].get(beperking_code)
+    def _get_indicator(self, indicator, field, value):
+        result = self.mapping[indicator][field].get(str(value))
+
         if not result:
             return None
-
         if not result.get('waarschuwingsniveau'):
             return None
-
         return result
+
+    def beperking_to_status_pand(self, beperking_code):
+        return self._get_indicator('Status pand', 'Beperking.beperkingcode', beperking_code)
+
+    def pand_status_to_status_pand(self, pand_status):
+        return self._get_indicator('Status pand', 'Pand.Pandstatus', pand_status)
--- web/zwaailicht/api/tests/test_mapping.py
+++ web/zwaailicht/api/tests/test_mapping.py
@@ -25,4 +25,27 @@ class MappingTest(TestCase):
         indicator = self.mapping.beperking_to_status_pand('DIT_IS_GEEN_CODE')
         self.assertIsNone(indicator)
 
+    def test_pand_status_24_status_pand(self):
+        indicator = self.mapping.pand_status_to_status_pand('24')
+        self.assertDictEqual({
+            'indicator': 'Status pand',
+            'waarschuwingsniveau': 2,
+            'label': 'Pand in bouwfase',
+            'aanvullende_informatie': 'Pand in bouwfase [Status]',
+        }, indicator)
+
+    def test_pand_status_number_24_status_pand(self):
+        indicator_str = self.mapping.pand_status_to_status_pand('24')
+        indicator_int = self.mapping.pand_status_to_status_pand(24)
+        self.assertDictEqual(indicator_str, indicator_int)
+
+    def test_pand_status_31(self):
+        self.assertIsNone(self.mapping.pand_status_to_status_pand(31))
+
+    def test_pand_status_unknown(self):
+        self.assertIsNone(self.mapping.pand_status_to_status_pand("IK_BEN_OOK_GEEN_CODE"))
+
+
+
+
 
