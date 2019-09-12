cc35b818f4f5a6877ccb317406a0d1893f6daa29
Jeffrey Klardie <j.klardie@amsterdam.nl>
2019-09-12 13:57:10 +0200

    Renamed ENV var to SECRET KEY

--- web/zwaailicht/zwaailicht/settings.py
+++ web/zwaailicht/zwaailicht/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 insecure_key = 'insecure'
-SECRET_KEY = os.environ['HANDELSREGISTER_SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = SECRET_KEY == insecure_key
 
